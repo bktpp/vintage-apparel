5b44240da14026b273423fea693ea7b063a9b23f
bktpp <117415722+bktpp@users.noreply.github.com>
2023-10-27 16:34:21 -0500

    created authentication page component alongside sign in form

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
    signInWithPopup,
    GoogleAuthProvider,
    createUserWithEmailAndPassword,
+   signInWithEmailAndPassword,
 } from "firebase/auth";
 
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
@@ -67,8 +68,14 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInfo = {}) 
    //return userdocref
 };
 
-export const createAuthUserWithEmailAndPassword = async (email, password, displayName) => {
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
    if (!email || !password) return;
 
    return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+   if (!email || !password) return;
+
+   return await signInWithEmailAndPassword(auth, email, password);
 };
